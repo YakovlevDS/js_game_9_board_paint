87410d305eb070c87009c80de5e4b1d13fde69c6
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-09-28 10:45:27 +0200

    added variable with check

--- app.js
+++ app.js
@@ -1,13 +1,57 @@
 const board = document.querySelector('#board')
-const SQUARES_NUMBER = 500
-for (let i = 0; i < SQUARES_NUMBER; i++){
-    const square = document.createElement('div')
-    square.classList.add('square')
-    square.addEventListener('mouseover', () => 
-        setColor(square))
-    
-board.append(square);
+let squareTotalInRow = +prompt("Введите количество ячеек в строке от 30 до 40", 40);
+
+if (!(squareTotalInRow > 29 && squareTotalInRow < 41)){
+  alert("Вы ввели некорректное число ячеек. Поэтому устанавливаем 35");
+  squareTotalInRow = 35;
+}
+
+console.log(squareTotalInRow);
+const squareBackgroundMain = 'Magenta';
+
+const squareSizeWMargin = squareTotalInRow * squareTotalInRow/100;
+const squareTotal = squareTotalInRow * squareTotalInRow;
+const squareMargin = squareSizeWMargin / 10;
+const squareSize = squareSizeWMargin -(2 * squareMargin);
+
+const boardMaxWidth = (squareTotalInRow * (squareSize + squareSize / 4));
+const colors = [
+  "red",
+  "blue",
+  "green",
+  "yellow",
+  "pink",
+  "orange",
+  "purple",
+  "Cyan",
+  "Navy",
+];
+board.style.maxWidth = `${boardMaxWidth}px`;
+
+for (let i = 0; i < squareTotal; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  square.style.backgroundColor = squareBackgroundMain;
+  square.style.width = `${squareSize}px`;
+  square.style.height = `${squareSize}px`;
+  square.style.margin = `${squareMargin}px`;
+
+  square.addEventListener("mouseover", () => setColor(square));
+  square.addEventListener("mouseleave", () => removeColor(square));
+  board.append(square);
 }
 function setColor(el) {
-    el.style.backgroundColor='red'
+    const color = getRandomColor(); 
+    el.style.backgroundColor = color
+    el.style.boxShadow=`0 0 2px ${color}, 0 0 10px ${color}`;
+
+}
+function removeColor(el) {
+    el.style.backgroundColor = squareBackgroundMain;
+    el.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length);
+    return colors[index]
 }
